bfddeeff3f310e665f47d2d9108ece4ea3c13a04
Ivan <volocivan@yandex.ru>
2019-06-04 17:19:30 +0300

    Changes in values p1 and p2

--- Main.java
+++ Main.java
@@ -1,12 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-<<<<<<< HEAD
 		Person p1 = new Person("Voronov", "Evgeniy");
         Person p2 = new Person("Belova", "Valeria");
-=======
-		Person p1 = new Person("Lebedev", "Egor");
-		Person p2 = new Person("Belova", "Valeria");
->>>>>>> feature1
         Person p3 = new Person("Ivanov", "Mark");
         Company c1 = new Company("Roga&Kopyta");
         c1.setBoss(p3);
